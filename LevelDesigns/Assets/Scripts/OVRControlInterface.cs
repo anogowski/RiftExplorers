8619e45fef03a620a83d91ec59cb7d7f412ee43f
Endrophes <alexanderba@charter.net>
2015-01-10 14:35:09 -0700

    Added OVR and Testing

---
+++ LevelDesigns/Assets/Scripts/OVRControlInterface.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OVRControlInterface : MonoBehaviour {
+
+    //public GameObject player;
+
+    public float rotationRachet;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        bool jump    = Input.GetKey( KeyCode.Space );
+        bool rotateR = Input.GetKey( KeyCode.E     );
+        bool rotateL = Input.GetKey( KeyCode.Q     );
+
+        OVRPlayerController OVRContr = gameObject.GetComponent("OVRPlayerController") as OVRPlayerController;
+
+        if (jump)
+        {
+            OVRContr.Jump();
+        }
+
+        if (rotateR)
+        {
+            OVRContr.YRotation += rotationRachet;
+            //Debug.Log("rotateR");
+        }
+
+        if (rotateL)
+        {
+            OVRContr.YRotation -= rotationRachet;
+           // Debug.Log("rotateL");
+        }
+
+	}
+}
---
+++ LevelDesigns/Assets/Scripts/OVRController.cs
@@ -0,0 +1,34 @@
+﻿/************************************************************************************
+
+ This class design is based on the Oculus VR OVRPlayerController
+ All rights are reserved by Copyright of Oculus VR, LLC.
+
+************************************************************************************/
+
+using UnityEngine;
+using System.Collections;
+
+public class OVRController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void updateMovement()
+    {
+        bool moveForward = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
+        bool moveLeft = Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow);
+        bool moveRight = Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow);
+        bool moveBack = Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow);
+        bool run = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+        bool jump = Input.GetKey(KeyCode.Space);
+
+        bool dpad_move = false;
+    }
+}
