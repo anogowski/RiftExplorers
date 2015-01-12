d4adc1cbff508227bc46c63ac148ff54ecea2368
Ansem571 <jadams.macdonnell1@gmail.com>
2015-01-12 09:11:34 -0700

    Adding in hallway. Fade in, fade out texture.

---
+++ LevelDesigns/Assets/Scripts/WaterToHallDoor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterToHallDoor : MonoBehaviour {
+
+    public GameObject doorToHall;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(SceneFadeInOut.sceneFade)
+        {
+            doorToHall.transform.position += new Vector3(0f, 0.001f, 0f);
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.Equals("Player") || other.gameObject.name.Equals("OVRPlayer") &&
+            (Appear.Triggered))
+        {
+            SceneFadeInOut.sceneFade = true;
+        }
+        
+    }
+}
