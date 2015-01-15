1eb7e68d73034ba7180d5462ded87ea2a9d3f331
anogowski <anogowski.neumont@gmail.com>
2015-01-15 11:47:01 -0800

    retextured walls working on event script

---
+++ LevelDesigns/Assets/Scripts/EventListener.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventListener : MonoBehaviour
+{
+   public Events e;
+    public EventListener(Events events)
+    {
+        e = events;
+    }
+
+     public static void fireEvent(EventListener el)
+    {
+         switch (el.e)
+        {
+            case Events.Death:
+                 
+                break;
+            case Events.CheckPoint:
+                
+                break;
+            default:
+                break;
+        }
+    }
+
+}
---
+++ LevelDesigns/Assets/Scripts/EventTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class EventTrigger : MonoBehaviour 
+{
+
+    
+
+   // private void send(Event msg)
+   //{
+   //    foreach (var item in events)
+   //    {
+   //        EventListener.fireEvent(item);
+   //    }
+   //}
+
+}
