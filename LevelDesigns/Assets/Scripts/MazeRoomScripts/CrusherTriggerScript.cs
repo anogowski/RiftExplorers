359f0857c3c1df2526fcaf2050954c8e84a4a2e6
Ansem571 <jadams.macdonnell1@gmail.com>
2015-01-21 10:44:42 -0700

    Committing traps and changes to maze room

---
+++ LevelDesigns/Assets/Scripts/MazeRoomScripts/CrusherTriggerScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrusherTriggerScript : MonoBehaviour {
+
+    public GameObject prefab;
+
+    void Start()
+    {
+        //prefab.SetActive(false);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.Equals("Player") || other.gameObject.name.Equals("OVRPlayer"))
+        {
+            //prefab.SetActive(true);
+            Rigidbody r = prefab.AddComponent<Rigidbody>();
+            r.constraints = RigidbodyConstraints.FreezePositionX |
+                RigidbodyConstraints.FreezePositionZ |
+                RigidbodyConstraints.FreezeRotationX |
+                RigidbodyConstraints.FreezeRotationY |
+                RigidbodyConstraints.FreezeRotationZ;
+            r.velocity = new Vector3(0f, -10f, 0f);
+        }
+    }
+}
