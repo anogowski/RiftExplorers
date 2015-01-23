ff9f557ec8a1ba58ed96ac104ceb17be2b9c6f16
Ansem571 <jadams.macdonnell1@gmail.com>
2015-01-22 19:09:47 -0700

    IT WORKS ... KINDA ... enough for Jaime

---
+++ LevelDesigns/Assets/Scripts/TempStuff/TempDoorToMaze.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TempDoorToMaze : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {     
+        if (other.gameObject.name.Equals("Player") || other.gameObject.name.Equals("OVRPlayer"))
+        {
+            Debug.Log("ToTheMaze");
+            Application.LoadLevel("MazeRoom");
+        }
+    }
+}
