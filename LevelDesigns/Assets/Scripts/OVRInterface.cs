a4fcd8912e0a335dcec7c1e8b9239e7f45181604
Endrophes <alexanderba@charter.net>
2015-01-25 13:35:26 -0700

    Adjusted OVR GUI, fixed picking up objects to grab in the cross hair

--- LevelDesigns/Assets/Scripts/OVRInterface.cs
+++ LevelDesigns/Assets/Scripts/OVRInterface.cs
@@ -17,6 +17,7 @@ public class OVRInterface : MonoBehaviour
     private OVRCameraRig cameraRig;
     GameObject Left_Hand;
     GameObject Right_Hand;
+    GameObject CenterEye;
     OVRPlayerController OVRContr;
 
     // Use this for initialization
@@ -26,6 +27,7 @@ public class OVRInterface : MonoBehaviour
        OVRContr  = this.transform.gameObject.GetComponent<OVRPlayerController>();
        Left_Hand = GameObject.FindGameObjectWithTag("Left_Hand");
        Right_Hand = GameObject.FindGameObjectWithTag("Right_Hand");
+       CenterEye = GameObject.FindGameObjectWithTag("CenterEye");
     }
 
     public void pickUP(Hand hand, GameObject obj)
@@ -33,28 +35,55 @@ public class OVRInterface : MonoBehaviour
         if (hand == Hand.Left)
         {
             obj.transform.parent = Left_Hand.transform;
+            obj.transform.localPosition = Vector3.zero;
+            obj.transform.localRotation = Quaternion.identity;
         }
         else if(hand == Hand.right)
         {
             obj.transform.parent = Left_Hand.transform;
+            obj.transform.localPosition = Vector3.zero;
+            obj.transform.localRotation = Quaternion.identity;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+        keyboardInput();
         movement();
+    }
+
+    private void keyboardInput()
+    {
+        bool escape = Input.GetKey(KeyCode.Escape) || OVRGamepadController.GPC_GetButton(OVRGamepadController.Button.Start);
+        if(escape)
+        {
+
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            #else
+            Application.Quit();
+            #endif
+        }
     }
 
     public Vector3 getForward()
     {
-        return cameraRig.centerEyeAnchor.forward;
+        //return cameraRig.centerEyeAnchor.forward;
+        Vector3 forward = CenterEye.transform.forward;
+        Debug.Log("Forward Vector 3: " + forward);
+        return forward;
+    }
+
+    public Vector3 getPosition()
+    {
+        return CenterEye.transform.position;
     }
 
     //Interface to OVR Controller
     private void movement()
     {
-        bool jump = Input.GetKey(KeyCode.Space);
+        bool jump = Input.GetKey(KeyCode.Space) || OVRGamepadController.GPC_GetButton(OVRGamepadController.Button.Start);
         bool rotateR = Input.GetKey(KeyCode.E);
         bool rotateL = Input.GetKey(KeyCode.Q);
 
