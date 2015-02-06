96d8e03e0dae902699e1a51279e8c5f53fb84337
anogowski <anogowski.neumont@gmail.com>
2015-02-05 19:00:16 -0700

    plank models, wood and grass texture

---
+++ LevelDesigns/Assets/Scripts/Text/AIMHERE.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIMHERE : DisplayText {
+
+	// Use this for initialization
+	void Start () 
+    {
+        textToDisplay = "AIM\nHERE\nV";
+	}
+	
+}
