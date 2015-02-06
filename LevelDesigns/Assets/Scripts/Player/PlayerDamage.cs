e00e402544e38b6b3effa43610e8a7a724640004
anogowski <anogowski.neumont@gmail.com>
2015-02-05 20:46:52 -0700

    script and material

---
+++ LevelDesigns/Assets/Scripts/Player/PlayerDamage.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDamage : MonoBehaviour {
+
+    bool played = false;
+    public static bool dead = false;
+    int count = 0;
+
+
+    IEnumerator playerHurt()
+    {
+        if (!played)
+        {
+            played = true;
+        }
+        yield return new WaitForSeconds(2f);
+        played = false;
+        count++;
+            if (count >= 5)
+            {
+                dead = true;
+                count = 0;
+            }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
