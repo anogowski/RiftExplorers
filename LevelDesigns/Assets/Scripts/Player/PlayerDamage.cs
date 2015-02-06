73bba6132a3256aabd4f50add8f7025452550229
anogowski <anogowski.neumont@gmail.com>
2015-02-05 21:04:23 -0700

    script for player hurt done

--- LevelDesigns/Assets/Scripts/Player/PlayerDamage.cs
+++ LevelDesigns/Assets/Scripts/Player/PlayerDamage.cs
@@ -1,11 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerDamage : MonoBehaviour {
-
+public class PlayerDamage : MonoBehaviour
+{
+    GameObject wave;
+    Transform waveHeight;
+    GameObject player;
+    Vector3 playerHeight;
+    public GameObject plane;
     bool played = false;
     public static bool dead = false;
     int count = 0;
+
+    void Start()
+    {
+        wave = GameObject.FindGameObjectWithTag("Water");
+        waveHeight = wave.transform.FindChild("Wave");
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHeight = player.transform.position;
+    }
 
 
     IEnumerator playerHurt()
@@ -13,20 +26,35 @@ public class PlayerDamage : MonoBehaviour {
         if (!played)
         {
             played = true;
+            plane.SetActive(true);
+            AudioManager.Instance.PlaySounds(Sounds.Player_Hurt, SoundActions.Play, plane.transform.position);
         }
         yield return new WaitForSeconds(2f);
         played = false;
+        plane.SetActive(false);
         count++;
-            if (count >= 5)
-            {
-                dead = true;
-                count = 0;
-            }
+        if (count >= 5)
+        {
+            dead = true;
+            count = 0;
+        }
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
-	
-	}
+        findHeight();
+        if (playerHeight.y < waveHeight.position.y )
+        {
+            playerHurt();
+        }
+    }
+
+    void findHeight()
+    {
+        wave = GameObject.FindGameObjectWithTag("Water");
+        waveHeight = wave.transform.FindChild("Wave");
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHeight = player.transform.position;
+    }
 }
--- LevelDesigns/Assets/Scripts/SoundSystem/Sounds.cs
+++ LevelDesigns/Assets/Scripts/SoundSystem/Sounds.cs
@@ -2,5 +2,5 @@
 
 public enum Sounds
 {
-    Hallelujah, WaterTempleTheme,WaterTempleComplete, WaterTempleDeath, Water, Waves, GetItem, Clang, Chain, JamieScream, Valve, 
+    Hallelujah, WaterTempleTheme,WaterTempleComplete, WaterTempleDeath, Water, Waves, GetItem, Clang, Chain, JamieScream, Valve, Player_Hurt
 }
