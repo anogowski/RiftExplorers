286eba6e7b2739c96515d98de6e0ca8c61ef1da3
anogowski <anogowski.neumont@gmail.com>
2015-04-16 16:10:31 -0600

    voice POC WORKS

--- LevelDesigns/Assets/Scripts/UI/FadingManager.cs
+++ LevelDesigns/Assets/Scripts/UI/FadingManager.cs
@@ -20,11 +20,11 @@ public class FadingManager : MonoBehaviour {
     public void Update()
     {
         //Debug.Log("Alpha: " + planePrefab.renderer.material.color.a);
-        if((fadingOut) && planePrefab.renderer.material.color.a <= 1f)
+        if((fadingOut) && planePrefab.GetComponent<Renderer>().material.color.a <= 1f)
         {
             FadeOutScene();
         }
-        if (fadingIn && planePrefab.renderer.material.color.a >= 0f)
+        if (fadingIn && planePrefab.GetComponent<Renderer>().material.color.a >= 0f)
         {
             FadeInScene();
         }
@@ -33,14 +33,14 @@ public class FadingManager : MonoBehaviour {
     void FadeToClear()
     {
         //Debug.Log("Fading to clear" + planePrefab.renderer.material.color.a);
-        planePrefab.renderer.material.color = 
-            Color.Lerp(planePrefab.renderer.material.color, Color.clear, speed * Time.deltaTime);
+        planePrefab.GetComponent<Renderer>().material.color = 
+            Color.Lerp(planePrefab.GetComponent<Renderer>().material.color, Color.clear, speed * Time.deltaTime);
     }
     void FadeToBlack()
     {
         //Debug.Log("Fading to black" + planePrefab.renderer.material.color.a);
-        planePrefab.renderer.material.color =
-            Color.Lerp(planePrefab.renderer.material.color, Color.black, speed * Time.deltaTime);
+        planePrefab.GetComponent<Renderer>().material.color =
+            Color.Lerp(planePrefab.GetComponent<Renderer>().material.color, Color.black, speed * Time.deltaTime);
     }
 
     public void FadeInScene()
@@ -53,15 +53,15 @@ public class FadingManager : MonoBehaviour {
         //{
         //    player.GetComponent<OVRPlayerController>().enabled = true;
         //}
-        if(planePrefab.renderer.material.color.a < 0.06f)
+        if(planePrefab.GetComponent<Renderer>().material.color.a < 0.06f)
         {
-            Color c = planePrefab.renderer.material.color;
-            planePrefab.renderer.material.color = new Color(c.r, c.g, c.b, 0.05f);
+            Color c = planePrefab.GetComponent<Renderer>().material.color;
+            planePrefab.GetComponent<Renderer>().material.color = new Color(c.r, c.g, c.b, 0.05f);
         }
-        if(planePrefab.renderer.material.color.a <= 0.05f)
+        if(planePrefab.GetComponent<Renderer>().material.color.a <= 0.05f)
         {
             //Debug.Log("GO");
-            planePrefab.renderer.material.color = Color.clear;
+            planePrefab.GetComponent<Renderer>().material.color = Color.clear;
             fadingIn = false;
             planePrefab.SetActive(false);
         }
@@ -72,14 +72,14 @@ public class FadingManager : MonoBehaviour {
         if (!planePrefab.activeInHierarchy)
         {
             planePrefab.SetActive(true);
-            planePrefab.renderer.material.color = Color.clear;
+            planePrefab.GetComponent<Renderer>().material.color = Color.clear;
             player.GetComponent<OVRPlayerController>().enabled = false;
             //Debug.Log("Stop for exit");
         }
 
         FadeToBlack();
 
-        if(planePrefab.renderer.material.color.a >= 0.95f)
+        if(planePrefab.GetComponent<Renderer>().material.color.a >= 0.95f)
         {
             fadingOut = false;
             fadingIn = true;
